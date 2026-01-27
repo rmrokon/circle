84d889bd26f78a74c5ca2fe75d20f2d5f01e549c
Md Roknuzzaman <mdrokon7773@gmail.com>
2026-01-27 22:47:47 +0600

    prod db setup added

--- backend/src/loaders/datasource.ts
+++ backend/src/loaders/datasource.ts
@@ -1,13 +1,13 @@
 import { Sequelize } from '@sequelize/core';
 import { PostgresDialect } from '@sequelize/postgres';
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, CA_CERT } = process.env;
 
-if (!DB_NAME || !DB_USER || !DB_PASSWORD || !DB_HOST || !DB_PORT)
+if (!DB_NAME || !DB_USER || !DB_PASSWORD || !DB_HOST || !DB_PORT || !CA_CERT)
   throw new Error(
-    `DB env variables are missing, required variables are - DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT`,
+    `DB env variables are missing, required variables are - DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, CA_CERT`,
   );
 
-export const sequelize = new Sequelize({
+export const sequelize = new Sequelize<PostgresDialect>({
   dialect: PostgresDialect,
   database: DB_NAME,
   user: DB_USER,
@@ -18,6 +18,10 @@ export const sequelize = new Sequelize({
   retry: {
     max: 3,
   },
+  ssl: {
+    rejectUnauthorized: true,
+    ca: CA_CERT,
+  },
   pool: {
     max: 3, // Reduce max connections per pod
     min: 1,
@@ -25,3 +29,5 @@ export const sequelize = new Sequelize({
     idle: 5000, // Reduce idle time to free up connections faster
   },
 });
+
+
